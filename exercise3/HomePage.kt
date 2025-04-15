67d6a9e5cd5a68365e704713bf2b4d2f81f2f491
machiwao <114682980+machiwao@users.noreply.github.com>
2025-04-15 18:26:35 +0800

    Add files via upload

---
+++ exercise3/HomePage.kt
@@ -0,0 +1,25 @@
+package com.example.exercise2
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class HomePage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_home_page)
+        val txtUsername = findViewById<TextView>(R.id.txtUsername)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            val loggedUser = intent.getStringExtra("loggedUsername")
+            txtUsername.setText(loggedUser)
+            insets
+        }
+    }
+}
